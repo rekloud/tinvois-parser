f08da3def827ea28119c333610d4aa999db64c18
Reza <srh.umir@gmail.com>
2020-11-14 22:04:40 +0100

    rename

--- app/test/test.py
+++ app/test/test.py
@@ -1,17 +1,19 @@
-import app.main.parser_servie.parse_image as pi
-from app.main.parser_servie.utils import read_config
+import app.main.parser.parse_image as pi
+from app.main.parser.utils import read_config
 import io
 
 # imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\3.jpeg'
 imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\alaki.jpg'
 imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\fileName.jpg'
 imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\fritten.jpg'
-imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\2.jpeg'
-imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\invoice1.jpeg'
+# imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\2.jpeg'
+# imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\invoice1.jpeg'
 imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\3.jpeg'
-imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\rossmann.jpg'
-imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\tank-oil.jpg'
-imgae_path = r'C:\Users\shossein\OneDrive\house\hoerde Stiftkamp 18\Rechnung\hornbach-26.03.2019.jpg'
+imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\kaufland_horiz.jpg'
+imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\kaufland_vertical.jpg'
+# imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\rossmann.jpg'
+# imgae_path = r'C:\Users\shossein\Documents\personal\taxapp\sample_invoices\tank-oil.jpg'
+# imgae_path = r'C:\Users\shossein\OneDrive\house\hoerde Stiftkamp 18\Rechnung\hornbach-26.03.2019.jpg'
 with io.open(imgae_path, 'rb') as f:
     image_content = f.read()
 
